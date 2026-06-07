45a65b27bce93e72dab356733e03b0865f20a447
Tiago Wagner de Oliveira Lopes <tiago.w@escolar.ifrn.edu.br>
2026-06-07 14:17:00 -0300

    Implementado vinculo aluno turma com List

--- src/main/java/br/edu/ifrn/sge/modelo/Turma.java
+++ src/main/java/br/edu/ifrn/sge/modelo/Turma.java
@@ -1,7 +1,13 @@
 package br.edu.ifrn.sge.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Turma {
-    // Atributos da Turma
+    // Atributos básicos
     public String nome;
     public String codigo;
+
+    // O Vínculo: Uma lista para guardar os alunos desta turma!
+    public List<Aluno> listaDeAlunos = new ArrayList<>();
 }
