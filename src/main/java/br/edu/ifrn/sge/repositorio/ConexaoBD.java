765a8618ef34e7083e300b1185d4cfd277c0921e
Tiago Wagner de Oliveira Lopes <tiago.w@escolar.ifrn.edu.br>
2026-07-14 06:44:05 -0300

    Corrigido nome do banco para sge_db

--- src/main/java/br/edu/ifrn/sge/repositorio/ConexaoBD.java
+++ src/main/java/br/edu/ifrn/sge/repositorio/ConexaoBD.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class ConexaoBD {
     // URL, usuário e senha do seu banco MySQL local
-    private static final String URL = "jdbc:mysql://localhost:3306/info3_sge";
+    private static final String URL = "jdbc:mysql://localhost:3306/sge_db";
     private static final String USUARIO = "root";
     private static final String SENHA = "admin"; 
 
