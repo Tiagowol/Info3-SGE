51bf217e6e32b185f17c76024c10fde755f98744
Tiago Wagner de Oliveira Lopes <tiago.w@escolar.ifrn.edu.br>
2026-07-14 06:39:59 -0300

    Senha do MySQL atualizada para admin e teste concluido

--- src/main/java/br/edu/ifrn/sge/repositorio/ConexaoBD.java
+++ src/main/java/br/edu/ifrn/sge/repositorio/ConexaoBD.java
@@ -8,7 +8,7 @@ public class ConexaoBD {
     // URL, usuário e senha do seu banco MySQL local
     private static final String URL = "jdbc:mysql://localhost:3306/info3_sge";
     private static final String USUARIO = "root";
-    private static final String SENHA = "root"; 
+    private static final String SENHA = "admin"; 
 
     public static Connection conectar() throws SQLException {
         return DriverManager.getConnection(URL, USUARIO, SENHA);
